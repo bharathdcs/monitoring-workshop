a2ffcc199acb07346269ce0f3e5a8221a67e93db
BHARATH KUMAR DEVARAJU <bkdevara@au1.ibm.com>
2024-06-19 16:49:27 +1000

    updasted the scripts

--- exercise1.sh
+++ exercise1.sh
@@ -2,9 +2,9 @@ oc apply -f - <<EOF
 apiVersion: v1
 kind: ConfigMap
 metadata:
-  name: user-workload-monitoring-config
-  namespace: openshift-user-workload-monitoring
+  name: cluster-monitoring-config
+  namespace: openshift-monitoring
 data:
   config.yaml: |
-    enableUserWorkload: true
+    enableUserWorkload: true 
 EOF
