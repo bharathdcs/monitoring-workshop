7b1535b7e47fb0cae92d711f5714b57ceef950d3
BHARATH KUMAR DEVARAJU <bkdevara@au1.ibm.com>
2024-07-03 06:41:33 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 export OCP_URL=https://api.dbk.cp.fyre.ibm.com:6443
 export OCP_USERNAME=admin
 export OCP_PASSWORD=passw0rd
+export PROJECT_CPD_INST_OPERANDS=zen
 #export OCP_TOKEN=<enter your token>
